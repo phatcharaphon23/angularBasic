61fb5caec69b755f563963600324ec4521b61275
phatcharaphon23 <phatcharaphon.h@ku.th>
2023-07-31 17:04:56 +0700

    add new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutDashboardComponent } from './layout-dashboard/layout-dashboard.component';
+import { LayoutLoginComponent } from './layout-login/layout-login.component';
+import { GlobalService } from './__service/global.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutDashboardComponent,
+    canActivate: [GlobalService],
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./layout-dashboard/layout-dashboard.module').then(
+            (m) => m.LayoutDashboardModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: LayoutLoginComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./layout-login/layout-login.module').then(
+            (m) => m.LayoutLoginModule
+          ),
+      },
+    ],
+  },
+  {
+    path:'**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
