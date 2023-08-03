8a30963d469110e0f9902f53ff7da1bb1d526ad1
phatchraphon23 <phatcharaphon.h@ku.th>
2023-08-03 17:02:21 +0700

    gg

--- src/app/__service/snack.service.ts
+++ src/app/__service/snack.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SnackService {
+  constructor(
+    private _snackBar: MatSnackBar,
+    
+    ) {}
 
-  constructor(private _snackBar :MatSnackBar) { }
-
-  openSnackBar( msg: string, msgType: string,timeout:number) {
+  openSnackBar(msg: string, msgType: string, timeout: number) {
     this._snackBar.open(msg, 'กดปิด', {
-      duration:timeout,
-      
+      duration: timeout,
+
       horizontalPosition: 'end',
       verticalPosition: 'top',
-    panelClass:[msgType]
+      panelClass: [msgType],
     });
   }
 }
